1e003c367b20e99668ff86ad4a7964951004fc40
Ignacio <ignaciovidalhernando@gmail.com>
2025-10-30 00:35:01 +0100

    Terminado 05_db_admin_exercises

---
+++ 03_Database/05_db_admin_exercises.sql
@@ -0,0 +1,26 @@
+/*
+
+1. Crea una base de datos llamada "cinedb_backup".
+
+    CREATE DATABASE cinedb_backup
+
+2. Elimina la base de datos llamada "cinedb_backup".
+
+    DROP DATABASE cinedb_backup
+
+3. Elimina, si existe, la base de datos llamada "cinedb_backup".
+
+    DROP DATABASE IF EXISTS cinedb_backup
+
+*/ 
+
+-- Soluciones
+
+-- 1. 
+CREATE DATABASE cinedb_backup;
+
+-- 2. 
+DROP DATABASE cinedb_backup;
+
+-- 3. 
+DROP DATABASE IF EXISTS cinedb_backup;
